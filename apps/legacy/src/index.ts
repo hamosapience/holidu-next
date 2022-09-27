6aa1ffad0d9adb2d8714d4041521ab743419651a
Valentin Kamenek <valentin.kamenek@holidu.com>
2022-09-27 19:22:10 +0200

    remove logger

--- apps/legacy/src/index.ts
+++ apps/legacy/src/index.ts
@@ -1,9 +1,7 @@
 import { createServer } from "./server";
-import { log } from "logger";
 
 const port = process.env.PORT || 3005;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`legacy running on ${port}`);
 });
